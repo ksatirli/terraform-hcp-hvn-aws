e7b99619e3e662e2af5d5d0668c3ac849c8ea9d3
Kerim Satirli <ksatirli@users.noreply.github.com>
2022-08-15 17:22:10 +0200

    Bugfixes (#2)
    * updates version constraints logic
    
    * adds link to examples
    
    * bumps AWS provider version to `4.26.0`

--- examples/basic/terraform.tf
+++ examples/basic/terraform.tf
@@ -1,16 +1,16 @@
 terraform {
   # see https://www.terraform.io/docs/language/settings/index.html#specifying-provider-requirements
   required_providers {
-    # see https://registry.terraform.io/providers/hashicorp/aws/4.25.0/
+    # see https://registry.terraform.io/providers/hashicorp/aws/4.26.0/
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 4.25.0"
+      version = ">= 4.26.0, < 5.0.0"
     }
 
-    # see https://registry.terraform.io/providers/hashicorp/hcp/0.39.0/
+    # see https://registry.terraform.io/providers/hashicorp/hcp/0.40.0/
     hcp = {
       source  = "hashicorp/hcp"
-      version = ">= 0.39.0"
+      version = "0.40.0, < 1.0.0"
     }
   }
 

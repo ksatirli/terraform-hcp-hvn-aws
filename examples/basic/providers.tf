ff06d445b9b26bfe112fff7065fa408fa17dc245
Kerim Satirli <k.satirli@gmail.com>
2022-08-30 04:45:51 +0200

    corrects typo

--- examples/basic/providers.tf
+++ examples/basic/providers.tf
@@ -1,4 +1,4 @@
-# The HCP Provider is set to retrieve configuration from the executing environment
+# The AWS Provider is set to retrieve configuration from the executing environment
 # see https://registry.terraform.io/providers/hashicorp/aws/latest/docs#schema
 provider "aws" {
   region = var.aws_region
